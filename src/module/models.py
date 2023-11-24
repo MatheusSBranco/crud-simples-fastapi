d30da5c13c193aad4877b1739f76ca06580d792f
MatheusSBranco <m.siebeneichler@gmail.com>
2023-11-24 10:13:33 -0300

    Validação de dados no model

--- src/module/models.py
+++ src/module/models.py
@@ -1,9 +1,9 @@
-from typing import Optional
+from typing import Optional, Annotated
 
 from sqlmodel import Field, SQLModel
 
 class Book(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    title: str
-    genre: str
-    release_age: int
+    title: Annotated[str, Field(min_length=3, max_length=50)]
+    genre: Annotated[str, Field(min_length=3, max_length=20)]
+    release_age: Annotated[int, Field(gt=0)]
